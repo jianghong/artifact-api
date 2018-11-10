641d7e1b37de757a283318181ef7f7b02ab99f03
Jackson Hong <jackson.hong@shopify.com>
2018-11-09 21:10:17 -0500

    Card set request with gross error handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,28 @@
+extern crate reqwest;
+
+const CARD_SET_REQUEST_URL: &str =  "https://playartifact.com/cardset/";
+
+pub const BASE_SET_ID: &str = "00";
+pub const CALL_TO_ARMS_SET_ID: &str = "01";
+
+#[derive(Debug)]
+pub enum CardSetRequestError {
+	InvalidSetID { err: reqwest::UrlError },
+	ReqwestError { err: reqwest::Error },
+}
+
+pub fn card_set_request(set_id: &str) -> Result<String, CardSetRequestError> {
+	parse_url(set_id)
+		.and_then(|url| reqwest::get(url).map_err(|e| CardSetRequestError::ReqwestError{err: e}))
+		.and_then(|mut response| response.text().map_err(|e| CardSetRequestError::ReqwestError{err: e}))
+		.and_then(|body| Ok(body))
+}
+
+fn parse_url(set_id: &str) -> Result<reqwest::Url, CardSetRequestError> {
+	reqwest::Url::parse(CARD_SET_REQUEST_URL).map_err(|e| CardSetRequestError::InvalidSetID{err: e})
+		.and_then(|base_url| base_url.join(set_id).map_err(|e| CardSetRequestError::InvalidSetID{err: e}))
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+extern crate artifact_api_rs;
+
+
 fn main() {
-	println!("Hello artifact-api-rs");
+	match artifact_api_rs::card_set_request(artifact_api_rs::BASE_SET_ID) {
+		Ok(response) => println!("{:?}", response),
+		Err(e) => println!("{:?}", e)
+	}
 }
